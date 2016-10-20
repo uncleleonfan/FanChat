e22a470c1ae15a7cbe585e93b899e7b62c2cb696
Leon <526247082@qq.com>
2016-10-20 11:10:46 +0800

    Add ime options for send button.

--- app/src/main/java/com/itheima/leon/qqdemo/ui/activity/ChatActivity.java
+++ app/src/main/java/com/itheima/leon/qqdemo/ui/activity/ChatActivity.java
@@ -1,6 +1,11 @@
 package com.itheima.leon.qqdemo.ui.activity;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,6 +26,12 @@ public class ChatActivity extends BaseActivity {
     ImageView mBack;
     @BindView(R.id.title)
     TextView mTitle;
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.edit)
+    EditText mEdit;
+    @BindView(R.id.send)
+    Button mSend;
 
     @Override
     public int getLayoutRes() {
@@ -34,14 +45,32 @@ public class ChatActivity extends BaseActivity {
         String userName = getIntent().getStringExtra(Constant.ExtraKey.USER_NAME);
         String title = String.format(getString(R.string.chat_with), userName);
         mTitle.setText(title);
+
+        mEdit.setOnEditorActionListener(mOnEditorActionListener);
     }
 
-    @OnClick(R.id.back)
+    @OnClick({R.id.back, R.id.send})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.back:
                 finish();
                 break;
+            case R.id.send:
+
+                break;
         }
     }
+
+    private TextView.OnEditorActionListener mOnEditorActionListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+
+                return true;
+            }
+            return false;
+        }
+    };
+
+
 }
